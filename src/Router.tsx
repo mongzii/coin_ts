da831e6ff88c180a7662593a9231fe86151bd871
moonjiyoung <umarg228@naver.com>
2023-08-25 23:20:15 +0900

    feat: useMatch적용했는데 잘 안되는듯...

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
-
+import Chart from "./routes/Chart";
+import Price from "./routes/Price";
 
 
 function Router(){
@@ -9,6 +10,8 @@ function Router(){
     <Routes>
         <Route path="/" element={<Coins />}/>
         <Route path="/:coinId" element={<Coin />} />
+        <Route path="/:coinId/chart" element={<Chart />}/>
+        <Route path="/:coinId/price" element={<Price />}/>
     </Routes>
     </BrowserRouter>
 }
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router";
+import { Outlet, Link, useMatch } from "react-router-dom";
 import { styled } from "styled-components";
+import Chart from "./Chart";
+
+
 
 
 interface RouterState {
@@ -68,6 +72,7 @@ interface IPriceData {
 }
 
 const Container = styled.div`
+/* border: 5px solid blue; */
     padding: 0px 20px;
     max-width: 480px;
     margin: 0 auto;
@@ -110,6 +115,26 @@ const OverviewItem = styled.div`
 const Description = styled.p`
     margin: 20px 0px;
 `;
+const Tabs = styled.div`
+    display: grid;
+    grid-template-columns: repeat()(2, 1fr);
+    margin: 25px 0px;
+    gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: rgba(0,0,0,0.5);
+    padding: 7px 0px;
+    border-radius: 10px;
+    color: ${props => props.isActive ? props.theme.accentColor : props.theme.textColor}
+    a {
+        display: block;
+    }
+`;
 // react-router-dom v6부터 제네릭을 지원하지 않아서 interface이름으로 넣으면 오류가 난다....
 /* interface RouteParams {
     coinId: string;
@@ -124,6 +149,10 @@ function Coin(){
      const {state} =  useLocation() as RouterState; 
     const [info, setInfo] = useState<IInfoData>();
     const [priceInfo, setPriceInfo] = useState<IPriceData>();
+    const priceMatch = useMatch("/:coinId/price");
+    const chartMatch = useMatch("/:coinId/chart");
+
+   
 
   useEffect(()=> {
     (async ()=> {
@@ -141,6 +170,7 @@ function Coin(){
   }, [])
 
     return  (
+      
     <Container>
     <Header>
     <Title>{state ? state : loading}</Title>
@@ -173,9 +203,23 @@ function Coin(){
                     <span>{priceInfo?.max_supply}</span>
                 </OverviewItem>
             </Overview>
+
+            <Tabs>
+                <Tab isActive={chartMatch !== null}>
+                    <Link to={`/${coinId}/chart`}>Chart</Link>
+                </Tab>
+                <Tab isActive={priceMatch !== null}>
+                    <Link to={`/${coinId}/price`}>Price</Link>
+                </Tab>
+            </Tabs>
+            
+            
         </>
     )} 
+    
+  
     </Container>
+    
     );
 }
 export default Coin;
