35a259704cf70838ce9c2afa6b602b2ec94fa961
moonjiyoung <umarg228@naver.com>
2023-08-27 01:17:21 +0900

    feat: 코인파프리카 유료되서 api주소 수정

--- src/api.ts
+++ src/api.ts
@@ -17,8 +17,10 @@ export function fetchCoinTickers(coinId: string | undefined){
 }
 
 export function fetchCoinHistory(coinId: string | undefined){
-    const endDate = Math.floor(Date.now() / 1000);
-    const startDate = endDate - 60*60*24*7*2;
-    return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`)
+    //coinpaprika가 유료되어서 니꼬가 자체 API만든걸로 함.
+    //const endDate = Math.floor(Date.now() / 1000);
+    //const startDate = endDate - 60*60*24*7*2;
+    //return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`)
+    return fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=btc-bitcoin`)
     .then((res)=> res.json());
 }
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -2,7 +2,16 @@
  import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
  
-
+interface IHistorical {
+    time_open: number;
+    time_close: number;
+    open: string;
+    high: string;
+    low: string;
+    close: string;
+    volume: string;
+    market_cap: number;
+}
 
 interface ChartProps {
     coinId: string;
@@ -10,7 +19,8 @@ interface ChartProps {
 
 function Chart(){
       const {coinId} = useParams();
-    const {isLoading, data} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+
+     const {isLoading, data} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
     return <h1>Chart</h1>
 }
 
